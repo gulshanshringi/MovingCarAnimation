81e611dd66c088975e8504794f7c632893c6039f
gulshanshringi <gulshanshringi0@gmail.com>
2021-06-07 21:06:06 +0530

    new module added

--- MovingCarAnimation/src/main/java/com/gulshanshringi/movingcaranimation/MapsUtils.java
+++ MovingCarAnimation/src/main/java/com/gulshanshringi/movingcaranimation/MapsUtils.java
@@ -1,45 +1,24 @@
 package com.gulshanshringi.movingcaranimation;
 
-import androidx.core.app.ActivityCompat;
-import androidx.core.content.ContextCompat;
-import androidx.fragment.app.FragmentActivity;
-
-import android.Manifest;
 import android.animation.ValueAnimator;
-import android.content.pm.PackageManager;
-import android.graphics.Bitmap;
 import android.graphics.Color;
-import android.graphics.drawable.BitmapDrawable;
-import android.location.Location;
 import android.os.AsyncTask;
-import android.os.Bundle;
 import android.os.Handler;
 import android.util.Log;
-import android.view.View;
 import android.view.animation.LinearInterpolator;
-import android.widget.Button;
-import android.widget.Toast;
 
-import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
-import com.google.android.gms.maps.OnMapReadyCallback;
-import com.google.android.gms.maps.SupportMapFragment;
-import com.google.android.gms.maps.model.BitmapDescriptorFactory;
-import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.LatLngBounds;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.gms.maps.model.PolylineOptions;
-import com.google.android.gms.maps.model.SquareCap;
 
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.io.BufferedReader;
-import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
@@ -49,68 +28,26 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 
-import static com.google.android.gms.maps.model.JointType.ROUND;
+public class MapsUtils {
 
-public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
-
-    private static final String API_KEY = "YOUR_KEY_HERE";
-    private static final int MY_PERMISSIONS_REQUEST_LOCATION = 101;
-    private static final long ANIMATION_SPEED = 500;
     private GoogleMap mMap;
+    private MapsUtils instance;
+
     private List<LatLng> points;
-    private Marker marker;
-    private int index;
-    private int next;
+    private String API_KEY;
+    private int index = -1;
+    private int next = 1;
     private LatLng startPosition;
     private LatLng endPosition;
-    private Bitmap bitMapMarker;
-    private Button startCarBtn;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_maps);
 
-        startCarBtn = findViewById(R.id.startCarBtn);
-
-        BitmapDrawable bitmapdraw = (BitmapDrawable) getResources().getDrawable(R.drawable.ic_car);
-        Bitmap b = bitmapdraw.getBitmap();
-        bitMapMarker = Bitmap.createScaledBitmap(b, 60, 60, false);
-
-        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
-        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
-                .findFragmentById(R.id.map);
-        mapFragment.getMapAsync(this);
+    public static MapsUtils getInstance(String api_key, GoogleMap mMap) {
+        return new MapsUtils(api_key, mMap);
     }
 
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
-    @Override
-    public void onMapReady(GoogleMap googleMap) {
-        mMap = googleMap;
-
-        // Add a marker in Sydney and move the camera
-        LatLng start = new LatLng(-33.96060815694196, 151.00217616999655);
-        LatLng dest = new LatLng(-33.952861957685315, 150.9978638958635);
-        mMap.addMarker(new MarkerOptions().position(start));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(start));
-        mMap.animateCamera(CameraUpdateFactory.zoomTo(16));
-
-        drawRoute(start, dest);
-
-        marker = mMap.addMarker(new MarkerOptions().position(start).
-                flat(true).icon(BitmapDescriptorFactory.fromBitmap(bitMapMarker)));
-    }
-
-    public void startCarAnimation(View view) {
-        showMovingCarAnimation();
+    public MapsUtils(String api_key, GoogleMap mMap) {
+        API_KEY = api_key;
+        this.mMap = mMap;
     }
 
     public void drawRoute(LatLng start, LatLng dest) {
@@ -121,9 +58,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         options.position(start);
         options.position(dest);
 
-        // Add new marker to the Google Map Android API V2
         mMap.addMarker(options);
-
 
         // Getting URL to the Google Directions API
         String url = getUrl(start, dest);
@@ -293,7 +228,6 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
             // Drawing polyline in the Google Map for the i-th route
             if (lineOptions != null) {
                 mMap.addPolyline(lineOptions);
-                startCarBtn.setVisibility(View.VISIBLE);
             }
         }
     }
@@ -378,11 +312,11 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         return poly;
     }
 
-    private void showMovingCarAnimation() {
-        startCarBtn.setVisibility(View.GONE);
-        Handler handler = new Handler();
+    private void showMovingCarAnimation(long animationSpeed, Marker marker) {
         index = -1;
         next = 1;
+        Handler handler = new Handler();
+
         handler.postDelayed(new Runnable() {
             @Override
             public void run() {
@@ -396,7 +330,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                     endPosition = points.get(next);
                 }
                 ValueAnimator valueAnimator = ValueAnimator.ofFloat(0, 1);
-                valueAnimator.setDuration(ANIMATION_SPEED);
+                valueAnimator.setDuration(animationSpeed);
                 valueAnimator.setInterpolator(new LinearInterpolator());
                 valueAnimator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
                     @Override
@@ -407,23 +341,19 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                         double lat = v * endPosition.latitude + (1 - v)
                                 * startPosition.latitude;
                         LatLng newPos = new LatLng(lat, lng);
+
                         marker.setPosition(newPos);
                         marker.setAnchor(0.5f, 0.5f);
                         marker.setRotation(getBearing(startPosition, newPos));
-//                        mMap.animateCamera(CameraUpdateFactory.newCameraPosition
-//                                (new CameraPosition.Builder().target(newPos)
-//                                        .zoom(15.5f).build()));
                         mMap.moveCamera(CameraUpdateFactory.newLatLng(newPos));
                     }
                 });
                 valueAnimator.start();
                 if (index != points.size() - 1) {
-                    handler.postDelayed(this, ANIMATION_SPEED);
-                } else {
-                    startCarBtn.setVisibility(View.VISIBLE);
+                    handler.postDelayed(this, animationSpeed);
                 }
             }
-        }, ANIMATION_SPEED);
+        }, animationSpeed);
     }
 
     private float getBearing(LatLng start, LatLng end) {
@@ -440,5 +370,4 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
             return (float) ((90 - Math.toDegrees(Math.atan(lng / lat))) + 270);
         return -1;
     }
-
 }
